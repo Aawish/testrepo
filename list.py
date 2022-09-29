d08eb77e1ce9186e13b80f3afe8a46f36dabc1aa
Aawish <78649066+Aawish@users.noreply.github.com>
2022-09-29 01:12:15 -0700

    Add files via upload
    list

---
+++ list.py
@@ -0,0 +1,12 @@
+l = [10, 20, 30, 40, 50, 'hello', 'sandy', 'baby']
+print(l[2])
+print(l[0:5])
+print(l[5:8])
+print(l[0::2])
+
+mon = ['no', 20, 'mahi', 'today', 90, 'kangaroo', 89, 50, 'hello', 'chatterbx']
+print(mon[1], mon[4], mon[6], mon[7])
+print(mon[0], mon[2], mon[3], mon[5], mon[8:10])
+print(mon[0:])
+print(mon[-1::-1])
+print(mon[-1::-2])
